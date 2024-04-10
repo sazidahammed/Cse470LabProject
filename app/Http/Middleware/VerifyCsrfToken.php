d7070db91386f49f76696b5d6f5bdd437ad0add3
sazidahammed <80714749+sazidahammed@users.noreply.github.com>
2024-04-10 12:00:14 +0600

    Update VerifyCsrfToken.php
    Sprint-4

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -11,7 +11,7 @@ class VerifyCsrfToken extends Middleware
      *
      * @var array<int, string>
      */
-    protected $except = [
-        //
+   protected $except = [
+        '/pay-via-ajax', '/success','/cancel','/fail','/ipn'
     ];
 }
