2bf3a35d64301d14f84078448ed25cf7bf085b54
sazidahammed <80714749+sazidahammed@users.noreply.github.com>
2024-04-10 11:56:53 +0600

    Update web.php
    sprint-4

--- routes/web.php
+++ routes/web.php
@@ -128,3 +128,17 @@ Route::get('/posts',function(){
 
 });
 require __DIR__.'/auth.php';
+
+// SSLCOMMERZ Start
+Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
+Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
+
+Route::post('/pay', [SslCommerzPaymentController::class, 'index']);
+Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
+Route::post('/success', [SslCommerzPaymentController::class, 'success']);
+Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
+Route::post('/cancel', [SslCommerzPaymentController::class, 'cancel']);
+
+Route::post('/ipn', [SslCommerzPaymentController::class, 'ipn']);
+//SSLCOMMERZ END
